afd317faf6297a149a7cc939fd32694479a30551
Atsunori Saito <sai@yedo.com>
2024-02-01 16:54:36 +0900

    chore: update copyright year to 2024 in various files

--- util/setup_zlac8030.sh
+++ util/setup_zlac8030.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2023, LexxPluss Inc.
+# Copyright (c) 2024, LexxPluss Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
